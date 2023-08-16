ec1d3c0663b3dd29e3db9398541379f8389a8fdf
Deedee <deedeeowoeye@gmail.com>
2023-08-16 20:41:26 +0100

    AI chatbot

--- passportSetup.js
+++ passportSetup.js
@@ -7,7 +7,7 @@ passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         // callbackURL: process.env.GOOGLE_CALLBACK_URL,
-        callbackURL: "/google/callback",
+        callbackURL: "http://localhost:3500/google/callback",
         passReqToCallback: true
     },
     function(request, accessToken, refreshToken, profile, done) {
